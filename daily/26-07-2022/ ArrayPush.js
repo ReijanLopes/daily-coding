183d0039a60ec98343924fcd18ef2a4315d1038f
ReijanLopes <56601947+ReijanLopes@users.noreply.github.com>
2022-07-27 13:48:15 -0300

    object Controller (#13)
    Co-authored-by: Reijan <reijan@Reijans-MBP.lan>

---
+++ daily/26-07-2022/ ArrayPush.js
@@ -0,0 +1,8 @@
+// ele adiciona um novo objeto dentro do array
+
+const myArray = [{name: "reijan", birthday: "22/12/1234"}, {name: "Jose", birthday: "22/12/1234"}]
+console.log(myArray) // array a cima
+
+myArray.push({name: "Marcos", birthday: "22/12/1234"})
+console.log(myArray)
+// [{name: "reijan", birthday: "22/12/1234"}, {name: "Jose", birthday: "22/12/1234"}, {name: "Marcos", birthday: "22/12/1234"}]
---
+++ daily/26-07-2022/ArrayShift.js
@@ -0,0 +1,4 @@
+// retira o primeiro objeto e o retorna
+const myArray = [{name: "reijan", birthday: "22/12/1234"}, {name: "Jose", birthday: "22/12/1234"}]
+const removeObject = myArray.shift()
+console.log(removeObject) // { name: 'reijan', birthday: '22/12/1234' }
