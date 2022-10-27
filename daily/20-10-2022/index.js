f11523e07de76ed97582e2073c3bff8f0c554a70
Reijan Lopes de Jesus <56601947+ReijanLopes@users.noreply.github.com>
2022-10-27 09:22:41 -0300

    20-10-2022 (#66)
    * 20-10-2022
    
    * cleaning code

---
+++ daily/20-10-2022/index.js
@@ -0,0 +1,34 @@
+const string =
+  "A quick brown fox jumped over the lazy dog. He is Sleeping on the couch. I think we should look up there. This has a spellngi error. His name is Tom. Let us get back to more Coding";
+const before = ["jumped", "Sleeping", "up", "spellngi", "Tom.", "Coding"];
+const afters = ["leaped", "sitting", "Down", "spelling", "john.", "algorithms"];
+
+const correctingAString = (string, before, after) => {
+  const stringSplit = string.split(" ");
+
+  const isCapitalized = (word) => {
+    return word[0] === word[0].toUpperCase();
+  };
+  const capitalizedWord = (word) => {
+    return word[0].toUpperCase() + word.slice(1);
+  };
+  const smallWord = (word) => {
+    return word[0].toLowerCase() + word.slice(1);
+  };
+
+  stringSplit.map((word, i) => {
+    before.map((before, index) => {
+      if (word === before) {
+        if (isCapitalized(word)) {
+          stringSplit[i] = capitalizedWord(after[index]);
+        } else {
+          stringSplit[i] = smallWord(after[index]);
+        }
+      }
+    });
+  });
+
+  return stringSplit.join(" ");
+};
+
+correctingAString(string, before, afters);
