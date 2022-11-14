ea23d2e8b3fd7fbea350de9a9386b0c0802656bd
ReijanLopes <56601947+ReijanLopes@users.noreply.github.com>
2022-11-14 10:44:32 -0300

    14-11-2022

---
+++ daily/14-11-2022/index.js
@@ -0,0 +1,61 @@
+const numbers = {
+  "ONE HUNDRED": 10000,
+  TWENTY: 2000,
+  TEN: 1000,
+  FIVE: 500,
+  ONE: 100,
+  QUARTER: 25,
+  DIME: 10,
+  NICKEL: 5,
+  PENNY: 1,
+};
+const oneHundred = 100;
+
+const cashRegister = (price, cash, cid) => {
+  let change = cash * oneHundred - price * oneHundred;
+
+  let cidTotal = 0;
+  for (let elem of cid) {
+    cidTotal += elem[1] * oneHundred;
+  }
+  if (change > cidTotal) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  } else if (change === cidTotal) {
+    return { status: "CLOSED", change: cid };
+  } else {
+    const answer = [];
+    cid = cid.reverse();
+
+    for (let elem of cid) {
+      const holder = [elem[0], 0];
+
+      elem[1] = elem[1] * oneHundred;
+
+      while (change >= numbers[elem[0]] && elem[1] > 0) {
+        change -= numbers[elem[0]];
+        elem[1] -= numbers[elem[0]];
+        holder[1] += numbers[elem[0]] / oneHundred;
+      }
+      if (holder[1] > 0) answer.push(holder);
+    }
+    if (change > 0) {
+      return { status: "INSUFFICIENT_FUNDS", change: [] };
+    }
+
+    return { status: "OPEN", change: answer };
+  }
+};
+
+console.log(
+  cashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
