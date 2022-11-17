159d038601d8719b8c198e9ecae0b362da3c4783
Reijan Lopes de Jesus <56601947+ReijanLopes@users.noreply.github.com>
2022-11-17 09:33:28 -0300

    09-11-2022 (#80)
    * 09-11-2022
    
    * clearCode

---
+++ daily/09-11-2022/index.js
@@ -0,0 +1,81 @@
+const root = document.querySelector(".root");
+const totalRow = 4;
+const totalColumn = 8;
+
+const createIds = () => {
+  return Array.from({ length: totalRow }).map((_, index) => {
+    const list = Array.from({ length: totalColumn })
+      .map((_, index) => {
+        return {
+          randomId:
+            Date.now().toString(36) +
+            Math.floor(
+              Math.pow(10, 12) + Math.random() * 9 * Math.pow(10, 12)
+            ).toString(36),
+          randomNumber: Math.floor(Math.random() * 4),
+        };
+      })
+      .flat();
+    return list;
+  });
+};
+
+const listIds = createIds();
+
+const filtered = (e) => {
+  const id = e?.target?.id ? e.target.id : e;
+  let res = undefined;
+  listIds.map((obj, indexRow) => {
+    obj.map((item, indexColumn) => {
+      if (item.randomId == id) {
+        res = { elem: item, indexRow: indexRow, indexColumn: indexColumn };
+      }
+    });
+  });
+  return res;
+};
+
+const handleClick = (e) => {
+  const { elem, indexRow, indexColumn } = filtered(e);
+  const array = [elem.randomId];
+
+  const checkKinshipLeft = (elem, indexRow, indexColumn) => {
+    if (indexColumn > 0 && indexColumn <= totalColumn) {
+      const elemLeft = listIds?.[indexRow]?.[indexColumn - 1];
+
+      if (elem.randomNumber == elemLeft.randomNumber) {
+        const { elem, indexRow, indexColumn } = filtered(elemLeft.randomId);
+        checkKinshipLeft(elem, indexRow, indexColumn);
+        array.unshift(elem.randomId);
+      }
+    }
+  };
+  checkKinshipLeft(elem, indexRow, indexColumn);
+};
+
+const createBox = () => {
+  listIds.map((item) => {
+    const containerBox = document.createElement("section");
+    containerBox.style.margin = "5px 0px";
+    containerBox.style.display = "flex";
+    containerBox.style.gap = "5px";
+    root.appendChild(containerBox);
+
+    item.map(({ randomId, randomNumber }) => {
+      const box = document.createElement("div");
+      box.id = randomId;
+      box.innerHTML = randomNumber;
+      box.value = randomNumber;
+      box.style.backgroundColor = "red";
+      box.style.width = "50px";
+      box.style.height = "50px";
+      box.style.display = "flex";
+      box.style.alignItems = "center";
+      box.style.justifyContent = "center";
+      containerBox.appendChild(box);
+      box.addEventListener("click", handleClick);
+    });
+  });
+};
+
+createBox();
