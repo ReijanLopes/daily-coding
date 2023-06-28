18b9980e959c62fa73f4c883eed64cfdadf56c67
Reijan Lopes <reijan.lj@gmail.com>
2023-06-28 09:16:45 -0300

    countNucleotides

---
+++ daily/28-06-2023/index.js
@@ -0,0 +1,12 @@
+function countNucleotides(strand) {
+  if (strand.match(/[^ACGT]/g) != null)
+    throw new Error("Invalid nucleotide in strand");
+
+  const dna = { A: 0, C: 0, G: 0, T: 0 };
+
+  for (let char of strand) {
+    dna[char] += 1;
+  }
+
+  return `${dna.A} ${dna.C} ${dna.G} ${dna.T}`;
+}
