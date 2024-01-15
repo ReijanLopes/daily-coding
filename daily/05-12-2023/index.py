b3123d0fa3d33d1158ec6270077a4958f177d66d
Reijan Lopes de Jesus <56601947+ReijanLopes@users.noreply.github.com>
2024-01-15 15:50:17 -0300

    Learning javascript and python day: 05-12-2023 (#123)

---
+++ daily/05-12-2023/index.py
@@ -0,0 +1,13 @@
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
+def bake_time_remaining(elapsed_bake_time):
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+def preparation_time_in_minutes(number_of_layers):
+    return number_of_layers * PREPARATION_TIME
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
+
+
+bake_time_remaining(10)
+preparation_time_in_minutes(20)
+elapsed_time_in_minutes(60,35)
