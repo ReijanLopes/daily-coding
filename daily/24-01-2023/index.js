81e79eaa54841f88d091639913841fa163e76db1
Reijan Lopes de Jesus <56601947+ReijanLopes@users.noreply.github.com>
2024-02-01 17:40:26 -0300

    24-01-2023 (#117)

---
+++ daily/24-01-2023/index.js
@@ -0,0 +1,20 @@
+const ROMAN_NUMERALS = [
+  [1000, "M"],
+  [900, "CM"],
+  [500, "D"],
+  [400, "CD"],
+  [100, "C"],
+  [90, "XC"],
+  [50, "L"],
+  [40, "XL"],
+  [10, "X"],
+  [9, "IX"],
+  [5, "V"],
+  [4, "IV"],
+  [1, "I"],
+];
+export const toRoman = (int) => {
+  if (int === 0) return "";
+  const [value, numeral] = ROMAN_NUMERALS.find(([value]) => value <= int);
+  return numeral + toRoman(int - value);
+};
