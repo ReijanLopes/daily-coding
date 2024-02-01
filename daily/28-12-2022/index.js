faa8475d482b22ac5035f7618267ea213be6fb9e
Reijan Lopes de Jesus <56601947+ReijanLopes@users.noreply.github.com>
2024-02-01 17:37:50 -0300

    28-12-2022 (#103)

---
+++ daily/28-12-2022/index.js
@@ -0,0 +1,19 @@
+const isSilence = (message) => message.trim().length === 0;
+const isShouting = (message) =>
+  /[a-z]/i.test(message) && message === message.toUpperCase();
+const isQuestion = (message) => message.trim().endsWith("?");
+const isShoutingQuestion = (message) =>
+  isShouting(message) && isQuestion(message);
+const isDefault = () => true;
+
+const rules = [
+  { test: isSilence, response: "Fine. Be that way!" },
+  { test: isShoutingQuestion, response: "Calm down, I know what I'm doing!" },
+  { test: isShouting, response: "Whoa, chill out!" },
+  { test: isQuestion, response: "Sure." },
+  { test: isDefault, response: "Whatever." },
+];
+
+const hey = (message) => rules.find(({ test }) => test(message)).response;
+
+hey("Does this cryogenic chamber make me look fat?");
